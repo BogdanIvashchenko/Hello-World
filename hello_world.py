88246cc6a6be26a338e6c16cc7da546f6ee4abc7
BogdanIvashchenko <bogdan.ivashchenko@gmail.com>
2017-10-11 17:10:42 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,10 @@
 import time
-from boto.s3.connection import S3Connection
+import os
 
-s3 = S3Connection(os.environ['GLOBAL_VARIABLE_STRING'])
+is_prod = os.environ.get('GLOBAL_VARIABLE_STRING', None)
+
+#from boto.s3.connection import S3Connection
+#s3 = S3Connection(os.environ['GLOBAL_VARIABLE_STRING'])
 
 print(GLOBAL_VARIABLE_STRING)
 time.sleep(60)
